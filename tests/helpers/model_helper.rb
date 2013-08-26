8947d2551f295c53a34292ca0633146c7ae5e3aa
Kyle Rames <kyle.rames@rackspace.com>
2013-08-26 14:05:07 -0500

    [core] added an begin/ensure to collection and model helpers to ensure that resources are properly deleted if an error occurs during testing.

--- tests/helpers/model_helper.rb
+++ tests/helpers/model_helper.rb
@@ -2,22 +2,23 @@ def model_tests(collection, params = {}, mocks_implemented = true)
 
   tests('success') do
 
-    @instance = collection.new(params)
+    begin
+      @instance = collection.new(params)
 
-    tests("#save").succeeds do
-      pending if Fog.mocking? && !mocks_implemented
-      @instance.save
+      tests("#save").succeeds do
+        pending if Fog.mocking? && !mocks_implemented
+        @instance.save
+      end
+
+      if block_given?
+        yield
+      end
+    ensure
+      tests("#destroy").succeeds do
+        pending if Fog.mocking? && !mocks_implemented
+        @instance.destroy
+      end
     end
-
-    if block_given?
-      yield
-    end
-
-    tests("#destroy").succeeds do
-      pending if Fog.mocking? && !mocks_implemented
-      @instance.destroy
-    end
-
   end
 
 end
